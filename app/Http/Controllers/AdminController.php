9c5ca34ba87c9432874930e3bedf8c0f5d2506ff
ujwaldai <ujwalshakha@gmail.com>
2023-08-23 14:58:14 +0545

    login

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,13 +3,50 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
     
     public function AdminDashboard()
     {
-        return view('admin.Admin_Dashboard');
+        return view('admin.index');
+    }
+
+    public function AdminLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('admin/login');
+    }
+
+    public function AdminLogin()
+    {
+        return view('admin.Admin_login');
+    }
+
+    public function AdminRegister(Request $request)
+    {
+            $request->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+                'password' => ['required', 'confirmed'],
+            ]);
+    
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+    
+            return redirect('admin.Admin_Register');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -34,9 +34,13 @@ require __DIR__.'/auth.php';
 Route::middleware(['auth','role:admin'])->group(function()
 {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 }); //End Group Admin Middleware
 
 Route::middleware(['auth','role:agent'])->group(function()
 {
     Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
 }); //End Group Agent Middleware
+
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
+Route::get('/admin/Register', [AdminController::class, 'AdminRegister'])->name('admin.Register');
